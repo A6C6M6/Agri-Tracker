55c5452454a249d546a166fca166d4f70f1d4dca
ABHILASH CM <ikmdevikulamta@gmail.com>
2026-06-12 22:34:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Replace with your Supabase Project URL and Anon Key
 const SUPABASE_URL = "https://icdppzjhqpskmtertrbv.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIs2I";
+const SUPABASE_ANON_KEY = "eeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljZHBwempocXBza210ZXJ0cmJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNzM1NzQsImV4cCI6MjA5Njg0OTU3NH0.BLfuw0HOZO6hT3C4Xm0tdi_W4fE-_quJhYvqE6Yhl2I";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
