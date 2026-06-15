365df4622000875a8d5cb9f1c20c902cd2815870
ABHILASH CM <ikmdevikulamta@gmail.com>
2026-06-15 23:15:31 +0530

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -4,7 +4,9 @@ const SUPABASE_URL =
 const SUPABASE_ANON_KEY =
 "sb_publishable_4wk7hLvO7ZYE5Xo2j-K1Iw_ja4Pu5RZ";
 
-const supabase =
+/* Create global client */
+
+window.supabaseClient =
 window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
